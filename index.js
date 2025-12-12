f74afc42b535ee22f1e2b0e40acf7a89d4508e39
reygenteng <muhammadrizieq3232@gmail.com>
2025-12-12 19:50:06 +0700

    Update index.js

--- index.js
+++ index.js
@@ -20,16 +20,38 @@ app.get('/api-config.js', (req, res) => {
 
 // Route khusus untuk halaman produk
 app.get('/product/freefire', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'freefire/index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'freefire', 'index.html'));
 });
 
 app.get('/product/mobilelegends', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'mobilegends/index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'mobilegends', 'index.html'));
 });
 
-// 404 Handler
-app.use((req, res) => {
+// Redirect untuk URL yang salah
+app.get('/freefire', (req, res) => {
+    res.redirect('/product/freefire');
+});
+
+app.get('/mobilelegends', (req, res) => {
+    res.redirect('/product/mobilelegends');
+});
+
+// Handler untuk halaman utama (root)
+app.get('/', (req, res) => {
+    // Jika ada index.html di public, akan otomatis dilayani oleh express.static
+    // Tapi kita bisa tambahkan custom logic di sini jika perlu
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// 404 Handler - HARUS di paling akhir
+app.use((req, res, next) => {
     res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
+});
+
+// Error handler untuk server errors (500)
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).sendFile(path.join(__dirname, 'public', '500.html'));
 });
 
 // Ekspor app untuk Vercel
